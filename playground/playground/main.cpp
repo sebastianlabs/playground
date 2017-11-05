c2cb443ef8d5d92620eac3c1c1613e30620407bb
Sebastian Labs <s.labs@me.com>
2017-11-04 17:59:26 -0700

    changed to anne

--- playground/playground/main.cpp
+++ playground/playground/main.cpp
@@ -13,6 +13,6 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello Anne!\n";
     return 0;
 }
