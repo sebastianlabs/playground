fdc6db3d91a3849465a0132b3aff56150f8b3f00
Sebastian Labs <s.labs@me.com>
2017-11-04 17:46:32 -0700

    Added project files

---
+++ playground/playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Sebastian Labs on 11/4/17.
+//  Copyright © 2017 Sebastian Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
