f8c7745d18a7866c044283006811f74d5aeedf01
Sebastian Labs <s.labs@me.com>
2017-11-08 16:52:05 -0800

    Added MapFun

---
+++ playground/playground/MapFun.cpp
@@ -0,0 +1,9 @@
+//
+//  MapFun.cpp
+//  playground
+//
+//  Created by Sebastian Labs on 11/8/17.
+//  Copyright © 2017 Sebastian Labs. All rights reserved.
+//
+
+#include "MapFun.hpp"
---
+++ playground/playground/MapFun.hpp
@@ -0,0 +1,14 @@
+//
+//  MapFun.hpp
+//  playground
+//
+//  Created by Sebastian Labs on 11/8/17.
+//  Copyright © 2017 Sebastian Labs. All rights reserved.
+//
+
+#ifndef MapFun_hpp
+#define MapFun_hpp
+
+#include <stdio.h>
+
+#endif /* MapFun_hpp */
